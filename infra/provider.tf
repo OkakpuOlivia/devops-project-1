66be751b51f54b5fa7ba0ab9fb639e579dd37c39
okakpuolivia <oliviaokakpu@gmail.com>
2025-02-22 13:07:04 +0000

    commit changes

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,4 +1,4 @@
 provider "aws" {
   region                   = "eu-west-1"
-  #shared_credentials_files = ["C:\\Users\\dell\\Downloads\\okakpuolivia_accessKeys.csv"]
+  #shared_credentials_files = ["C:/Users/dell/Downloads/okakpuolivia_accessKeys.csv"]
 }
